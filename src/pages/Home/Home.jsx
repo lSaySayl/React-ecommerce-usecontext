7f6b677d12bfc79198c417abf9da9ac8dcce7789
QuarcissusDev <juacaramirezlo@gmail.com>
2023-07-24 21:19:01 -0500

    Generando CardSkeleton y grid para la presentación de los productos

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,27 @@
-
+import { useContext } from "react";
+import { ProductsContext } from "../../context/ProductsContext";
 import Card from "../../components/Card/Card";
 
-
 const Home = () => {
+  const { products } = useContext(ProductsContext);
 
-    return (
-        <>
-        <h1>Home</h1>
-        <Card/>
-
-        </>
-    );
-}
+  return (
+    <>
+      <h1>Home</h1>
+     <section className="grid grid-cols-5 grid- gap-4">
+     {products?.map((product) => (
+        <Card
+          key={product.id}
+          title={product?.title || "Nombre del Producto no disponible"}
+          image={product?.images.length > 0 ? product.images[0] : null}
+          category={product?.category?.name || "Categoría no disponible"}
+          alt={product?.title || ""}
+          price={product?.price || 0}
+        />
+      ))}
+     </section>
+    </>
+  );
+};
 
 export default Home;
-
